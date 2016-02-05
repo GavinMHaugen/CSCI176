27a50063c53e66fac4d4d338657bfaa3e2b78ce9
Gavin Haugen <gavinhaugen@mail.fresnostate.edu>
2016-02-05 14:13:41 -0800

    Forgot to save before commiting :P

--- Program1/main.cpp
+++ Program1/main.cpp
@@ -16,7 +16,28 @@ double Fibo_r(double n);
 
 int main()
 {
+    int pid, status //process id and status initialization
+    int p1[2], p2[2], p3[2], p4[2]; //pipe initialization
+    int value = 20; //value we will be using for the fibo func. We will have to use 3 diff values
+    double start, stop, Fibo_i_total, Fibo_r_total; //values to time the fibp functions
 
+    //initializing all of the pipes we need for this program
+    pipe(p1); //pipe 1. will talk to the recursive fibo func (child3)
+    pipe(p2); //pipe 2. will talk to the incremental fibo func (child2)
+    pipe(p3); //pipe 3. will give info to child1(controller) from recursive fibo func
+    pipe(p4); //pipe 4. will give info to child1(controller) from incremental fibo func
+
+    for(int i = 1; i <= 3; i++)
+    {
+        pid = fork();
+        if(pid == 0 && i == 1)
+        {
+            write(p1[1], &value, sizeof(value));
+            write(p2[1], &value, sizeof(value));
+            read(p3[0], &Fibo_r_total, sizeof(Fibo_r_total));
+            read(p4[0], &Fibo_i_total, sizeof(Fibo_i_total));
+        }
+    }
 
     return 0;
 }
