c3502e58311a4eca194885d4ae703a066390c11e
Gavin Haugen <gavinhaugen@mail.fresnostate.edu>
2016-02-05 10:39:13 -0800

    Finished the Fibonacci functions in iterative and recursive versions

---
+++ Program1/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <unistd.h> //for fork, pipe, and wait
+#include <cstdlib> //for exit(0)
+#include <cstring>
+
+using namespace std;
+
+double Fibo_i(double n);
+double Fibo_r(double n);
+
+int main()
+{
+
+
+    return 0;
+}
+
+double Fibo_i(double n)
+{
+    int p1 = 1, p2 = 1, temp;
+
+    if(n == 1 || n == 2)
+        return 1;
+
+    else
+    {
+        for(int i = 3; i <= n; ++i)
+        {
+            temp = p1 + p2;
+            p1 = p2;
+            p2 = temp;
+        }
+        return temp;
+    }
+}
+
+double Fibo_r(double n)
+{
+    if(n == 1 || n == 2)
+        return 1;
+    else
+        return(Fibo_r(n-1) + Fibo_r(n-2));
+}
